7724460df55c8f89907e4717cc9f10f0c3b23a97
jainjhalak <jainjhalak9491@gmail.com>
2026-02-02 18:29:13 +0530

    script fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -67,9 +67,10 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get(/.*/, (req,res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist" , "index.html"));
     })
 }
 
